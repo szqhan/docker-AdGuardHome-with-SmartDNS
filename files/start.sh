4ffa53de6a933094ae3166c3779b35281d4d6932
szqhan <59555166+szqhan@users.noreply.github.com>
2025-02-23 15:38:25 +0800

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 # open smartdns
-OPEN_SMARTDNS=${OPEN_SMARTDNS:=false}
+#OPEN_SMARTDNS=${OPEN_SMARTDNS:=false}
 
-if [ $OPEN_SMARTDNS = 'true' ]; then
-    if [ ! -f /opt/conf/smartdns.conf ]; then
-        cp -u /config.conf /opt/conf/smartdns.conf
-    fi
+#if [ $OPEN_SMARTDNS = 'true' ]; then
+#    if [ ! -f /opt/conf/smartdns.conf ]; then
+#        cp -u /config.conf /opt/conf/smartdns.conf
+#    fi
+#    exec /smartdns/sbin/smartdns -f -x -c /opt/conf/smartdns.conf &
+#fi
+
+# adguardhome
+#exec /opt/adguardhome/AdGuardHome -h 0.0.0.0 -c /opt/conf/AdGuardHome.yaml -w /opt/conf/work --no-check-update
+
+if [ ! -f /opt/conf/smartdns.conf ]; then
+    cp -u /config.conf /opt/conf/smartdns.conf
     exec /smartdns/sbin/smartdns -f -x -c /opt/conf/smartdns.conf &
 fi
 
-# adguardhome
-exec /opt/adguardhome/AdGuardHome -h 0.0.0.0 -c /opt/conf/AdGuardHome.yaml -w /opt/conf/work --no-check-update
